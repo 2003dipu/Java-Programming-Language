a2dd926d215c69da6ab9fb9a99b400834f468355
Dipu Singha <137743843+2003dipu@users.noreply.github.com>
2024-01-25 16:23:58 +0600

    Add files via upload
    Java Encryption Program

---
+++ Java Projects/JavaEncryptionProgram/Main.java
@@ -0,0 +1,16 @@
+// Project name : Java Encryption Program
+/*
+OTP = One Time Pad
+encryption technique that cannot be cracked, but requires the use of one-time pre-shared key the same size as, or
+longer than, the message being sent. In this technique, plaintext is paired with a random secret key(also referred
+ to as a non-time pad)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        EncryptionProgram cp = new EncryptionProgram();
+
+    }
+}
