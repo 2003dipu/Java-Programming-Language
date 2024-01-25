ed13d0b2c4955a33c19e38c77429b714101d722d
Dipu Singha <137743843+2003dipu@users.noreply.github.com>
2024-01-25 17:18:23 +0600

    Add files via upload
    ## MenuBar in JavaFX with Scene Builder
    
    A MenuBar is a UI element in JavaFX used to create hierarchical menus with multiple options. It allows users to access various functionalities of your application through dropdown menus and submenus. Scene Builder, the visual design tool for JavaFX, makes creating and customizing MenuBars easy and intuitive.
    
    ### Using MenuBar with Scene Builder:
    
    1. **Adding a MenuBar:** Drag and drop the "MenuBar" element from the Library panel onto your scene.
    2. **Creating Menus and MenuItems:**
        * Right-click on the MenuBar and select "Add Menu" to create the main menu items.
        * Right-click on a menu item and select "Add Menu Item" to create submenus and individual options.
        * Double-click on a menu item to edit its text.
    3. **Setting Menu Actions:**
        * Select a MenuItem and go to the Properties panel.
        * Under "onAction", enter the name of the Java method you want to be called when the menu item is clicked.
        * You can define this method in your controller class to handle the user interaction.
    
    ### Key Features:
    
    * **Nested Menus:** Create submenus for complex structures and organization.
    * **Customization:** Change text, font, styles, and icons of menus and items.
    * **Event Handling:** Define actions for each menu item click using Java methods.
    * **Graphical Editing:** Scene Builder allows visual positioning and editing of menus.

---
+++ JavaFX and Scene Builder/Menu Bar/MenuBar/src/Controller.java
@@ -0,0 +1,20 @@
+
+
+public class Controller {
+
+    public void newMethod(){
+        System.out.println("You selected the new Menu item");
+    }
+
+    public void language(){
+        System.out.println("You have selected language menuItem to select a language");
+    }
+
+    public void share(){
+        System.out.println("You have selected the share menu item to share this page");
+    }
+
+    public void copyEntirePage(){
+        System.out.println("You have copied this entire page");
+    }
+}
